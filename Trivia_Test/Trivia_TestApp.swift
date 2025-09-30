65ce5c372a95e003b18bf178d72fc75cc4e30064
Win <winhtuthan@gmail.com>
2025-09-30 15:17:39 +0700

    Initial Commit

---
+++ Trivia_Test/Trivia_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trivia_TestApp.swift
+//  Trivia_Test
+//
+//  Created by Win on 30/9/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct Trivia_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
