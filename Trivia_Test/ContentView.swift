5d17b084a783b9bc9fbf9373a1c354d44bb77953
New Contributor <victor.yokoshi@gmail.com>
2025-09-30 15:17:39 +0700

    Initial Commit

---
+++ Trivia_Test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Trivia_Test
+//
+//  Created by Win on 30/9/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
