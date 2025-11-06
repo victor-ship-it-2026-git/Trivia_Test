bbeff03da7c0854df834c16d92b11da3aff858f2
Victor <victor.yokoshi@gmail.com>
2025-11-06 23:15:31 +0700

    - Final fixes.

---
+++ Trivia_Test/Helpers/TransitionCoordinator.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+@MainActor
+class TransitionCoordinator: ObservableObject {
+    @Published var isTransitioning = false
+    
+    func performTransition(duration: Double = 0.3, _ action: @escaping () -> Void) {
+        guard !isTransitioning else { return }
+        
+        isTransitioning = true
+        
+        withAnimation(.spring(response: duration, dampingFraction: 0.8)) {
+            action()
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
+            self.isTransitioning = false
+        }
+    }
+}
